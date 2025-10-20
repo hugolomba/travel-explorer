41d306dcc53be0aaf30e94ef83d78176a5aa33fc
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-10-20 21:10:46 +0100

    modal

--- js/script.js
+++ js/script.js
@@ -37,32 +37,5 @@ cards.forEach((card) => {
 
     // Scroll to results section
     resultsSection.scrollIntoView({ behavior: "smooth" });
-
-    // // Change body class based on selected card for background effect
-
-    // // Remove previous card classes
-    // body.className = "";
-
-    // // Add the new class based on selected card
-    // body.classList.add(`card-${selectedCard}`);
   });
 });
-
-// // Function to add hover effect
-// function addHoverEffect(card, colorClass) {
-//   card.addEventListener("mouseenter", () => {
-//     card.classList.add(colorClass);
-//   });
-
-//   card.addEventListener("mouseleave", () => {
-//     card.classList.remove(colorClass);
-//   });
-// }
-
-// // Adding hover effects to each card with specific color classes
-// addHoverEffect(document.querySelector(".card-adventure"), "hover-adventure");
-// addHoverEffect(document.querySelector(".card-culture"), "hover-culture");
-// addHoverEffect(document.querySelector(".card-extreme"), "hover-extreme");
-// addHoverEffect(document.querySelector(".card-mindful"), "hover-mindful");
-// addHoverEffect(document.querySelector(".card-spiritual"), "hover-spiritual");
-// // You can add more cards and their respective hover effects as needed
