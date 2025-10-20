f0fe00566cc027100b698159fee284dccc35c5ad
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-10-20 15:30:17 +0100

    add eco result section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let selectedCard = null;
 
 // DOM selection
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".options-card");
+const resultsSection = document.getElementById("results-section");
+const resultsTitle = document.getElementById("results-title");
 const body = document.body;
 
 // Function to handle card selection
@@ -9,6 +11,34 @@ cards.forEach((card) => {
   card.addEventListener("click", (event) => {
     // Using dataset to get the experience type
     selectedCard = event.currentTarget.dataset.experience;
+
+    switch (selectedCard) {
+      case "eco":
+        resultsTitle.textContent = "EcoVoyage – Sustainable Adventures";
+        break;
+      case "culture":
+        resultsTitle.textContent = "CultureQuest – Food, Art & History";
+        break;
+      case "extreme":
+        resultsTitle.textContent = "WildTrails – Extreme Adventures";
+        break;
+      case "spiritual":
+        resultsTitle.textContent = "MystiQuest – Spiritual & Hidden Journeys";
+        break;
+      case "mindful":
+        resultsTitle.textContent = "MindfulGetaway – Wellness & Balance";
+        break;
+      default:
+        resultsTitle.textContent = "Available Destinations";
+    }
+
+    // Show results section
+    resultsSection.classList.remove("hidden");
+
+    // Scroll to results section
+    resultsSection.scrollIntoView({ behavior: "smooth" });
+
+    // // Change body class based on selected card for background effect
 
     // // Remove previous card classes
     // body.className = "";
